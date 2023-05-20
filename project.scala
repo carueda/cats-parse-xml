7a96350dbbf165e0f39717e1d06de490544f474f
Carlos Rueda <carueda@mbari.org>
2023-05-19 17:33:37 -0700

    add ci

--- project.scala
+++ project.scala
@@ -1,3 +1,3 @@
-//> using dep org.typelevel::cats-parse:0.3.9
-//> using test.dep org.scalatest::scalatest:3.2.16
-//> using dep com.lihaoyi::pprint:0.8.1
+//> using dep "org.typelevel::cats-parse:0.3.9"
+//> using test.dep "org.scalatest::scalatest:3.2.16"
+//> using dep "com.lihaoyi::pprint:0.8.1"
